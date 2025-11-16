57e41e4c10e750b760588e2045af8ff538266a7c
mayurkadam9833 <mayurkadam9833@gmail.com>
2025-11-16 17:07:18 +0530

    create function to load pdf file and convert into document, text spliitter to split text data into multiple chunks,download embeddings

--- src/helper.py
+++ src/helper.py
@@ -2,7 +2,7 @@ from langchain.document_loaders import DirectoryLoader,PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter  
 from langchain.embeddings import HuggingFaceEmbeddings  
 
-
+# This function will load pdf files from directory and convert into documnet
 def load_pdf_files(data): 
     loader=DirectoryLoader(
         data, 
@@ -12,11 +12,13 @@ def load_pdf_files(data):
     documents=loader.load()
     return documents  
 
+# This function will split textual data in multiple chunks 
 def text_splitter(data): 
     text_split=RecursiveCharacterTextSplitter(chunk_size=2000,chunk_overlap=500)
     text_chunk=text_split.split_documents(data)
     return text_chunk 
 
+# This function will download embedding sfrom huggingface to convert text data to vector
 def download_embeddings(): 
     embeddings=HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
     return embeddings 
