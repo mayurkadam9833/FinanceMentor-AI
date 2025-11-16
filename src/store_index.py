37a33d86b1ed35fac29cad766aee63c9ec798410
mayurkadam9833 <mayurkadam9833@gmail.com>
2025-11-16 17:10:37 +0530

    extracted data from pdf and store in vector datbase

--- src/store_index.py
+++ src/store_index.py
@@ -3,16 +3,19 @@ from src.helper import load_pdf_files,text_splitter,download_embeddings
 from langchain.chains.combine_documents import create_stuff_documents_chain 
 from langchain.chains.retrieval import create_retrieval_chain  
 
-
-
-
+# extract data from pdf files 
 extracted_data=load_pdf_files("Data/")
+
+# split text data into chunk
 text_chunk=text_splitter(extracted_data) 
+
+# download embeddings to convert text data to vector
 embeddings=download_embeddings()
 
-
+# directory to save vector database
 dir="vector_database"
 
+# creating vector database with huggingface embedding and save vectors locally
 vector_data=Chroma.from_documents(
     documents=text_chunk,
     persist_directory=dir, 
